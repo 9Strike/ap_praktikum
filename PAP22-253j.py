6f3eb2012bd66c29fd1ddee0b02d8f98b2f65b7f
jona ackerschott <jona.a99@gmail.com>
2019-02-27 13:09:47 +0100

    PAP22-253j: Update.

--- PAP22-253j.py
+++ PAP22-253j.py
@@ -2,10 +2,11 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import measure as ms
 from measure import npfarray as npf
-from measure import pi as π
+from measure import pi
 from measure import sqrt, exp, log10, ln
 import numpy as np
 import scipy.constants as cs
+from scipy.optimize import curve_fit
 
 ms.plt.rc('text', usetex=True)
 ms.plt.rc('font', family='serif')
@@ -18,9 +19,11 @@ titles = [
 
 # Constants
 rho_Pb = 11.34 * cs.gram / cs.centi**3
+rho_Al = 2.699 * cs.gram / cs.centi**3
 
 # Measurements of the counter tube
 r_c = 14 * cs.milli / 2
+l_c = 4 * cs.centi
 U0_c = 520
 d_U0_c = 10
 
@@ -51,8 +54,27 @@ n0_Sr = n0_Sr / t3_Sr
 n_Sr -= n0_Sr
 d_n_Sr = sqrt(d_n_Sr**2 + d_n0_Sr**2)
 
-ms.pltext.initplot(num=1, title=titles[0], xlabel=r'$d$ / mm', ylabel=r'$\lg(n)$ / (1/s)', scale='linlog')
-ms.pltext.plotdata(d_Sr / cs.milli, n_Sr, d_n_Sr)
+ms.pltext.initplot(num=1, title=titles[0], xlabel=r'$d$ / mm', ylabel=r'$\lg(n)$ / (1/s)', scale='linlog', fignum=True)
+ms.expreg(d_Sr / cs.milli, n_Sr, d_n_Sr, fit_range=range(len(d_Sr) - 3), plot=True)
+ms.pltext.initplot(num=2, title=titles[0], xlabel=r'$d$ / mm', ylabel=r'$n$ / (1/s)', scale='linlin', fignum=True)
+sl_Sr, d_sl_Sr, i_Sr, d_i_Sr = ms.linreg(d_Sr / cs.milli, n_Sr, d_n_Sr, fit_range=range(len(d_Sr) - 3, len(d_Sr)), plot=True)
+sl_Sr /= cs.milli
+d_sl_Sr /= cs.milli
+
+R_Sr = -i_Sr / sl_Sr
+d_R_Sr = R_Sr * sqrt((d_i_Sr / i_Sr)**2 + (d_sl_Sr / sl_Sr)**2)
+
+sigma_ES = 0.130 * cs.gram / cs.centi**2
+sigma_Sr = R_Sr * rho_Al + sigma_ES      # => E_Sr = (2.3 ± 0.8) MeV
+d_sigma_Sr = d_R_Sr * rho_Al
+
+print("Absorption of β-Radiation")
+print(ms.val("sl", sl_Sr * cs.milli, d_sl_Sr * cs.milli, unit='1/(mm s)', prefix=False))
+print(ms.val("i", i_Sr, d_i_Sr, unit='1/s', prefix=False))
+print(ms.val("R", R_Sr, d_R_Sr, unit='m'))
+print(ms.val("σ", sigma_Sr * cs.centi**2 / cs.gram, d_sigma_Sr * cs.centi**2 / cs.gram, unit='g/cm²'))
+print(ms.val("E", 2.3e6, 0.8e6, unit='eV'))
+print()
 
 # Measurement of γ-Radiation absorption, Co 60, UB 595
 a_Co = 150 * cs.milli
@@ -66,18 +88,20 @@ d_Co = np.arange(0.0, 5 * cs.milli * len(n_Co), 5 * cs.milli)
 n_Co = n_Co - n0
 d_n_Co = sqrt(d_n_Co**2 + d_n0**2)
 
-ms.pltext.initplot(num=2, title=titles[1], xlabel=r'$d$ / mm', ylabel=r'$\lg(n)$ / (1/s)', scale='linlog')
+ms.pltext.initplot(num=3, title=titles[1], xlabel=r'$d$ / mm', ylabel=r'$\lg(n)$ / (1/s)', scale='linlog', fignum=True)
 sl_Co, d_sl_Co, _, _ = ms.expreg(d_Co / cs.milli, n_Co, d_n_Co, plot=True)
 mu_Co = -sl_Co / cs.milli
 d_mu_Co = d_sl_Co / cs.milli
 mu_rho_Co = mu_Co / rho_Pb      # => E_Co = (1.45 ± 0.05) MeV
 d_mu_rho_Co = d_mu_Co / rho_Pb
 
+print("Absorption of γ-Radiation")
+print(ms.val("μ", mu_Co * cs.milli, d_mu_Co * cs.milli, unit='1/mm', prefix=False))
 print(ms.val("μ/ρ", mu_rho_Co * cs.gram / cs.centi**2, d_mu_rho_Co * cs.gram / cs.centi**2, unit='cm²/g', prefix=False))
-print()
+print(ms.val("E", 1.45e6, 0.05e6, unit='eV'))
+print()1/mm
 
 # Measurement of γ-Radiation activity, Co 60, UB 595
-l_c = 4 * cs.centi
 A_N_CoA = 3.7 * cs.mega
 T_CoA = (dt.datetime(2019, 2, 21) - dt.datetime(2012, 2, 2)).total_seconds()
 T_H_CoA = 5.27 * cs.year
@@ -126,7 +150,7 @@ n_Am = n_Am / t_Am
 p_Am = npf([0.5 * (p1_Am[i] + p2_Am[i]) for i in range(len(p1_Am))])
 d_p_Am = npf([np.max([0.5 * np.abs(p2_Am[i] - p1_Am[i]), 1]) for i in range(len(p1_Am))])
 
-ms.pltext.initplot(num=3, title=titles[2], xlabel=r'$p$ / Pa', ylabel=r'$n$ / (1/s)')
+ms.pltext.initplot(num=4, title=titles[2], xlabel=r'$p$ / Pa', ylabel=r'$n$ / (1/s)', fignum=True)
 sl_Am, d_sl_Am, i_Am, d_i_Am = ms.linreg(p_Am, n_Am, d_n_Am, d_p_Am, fit_range=range(5,9), plot=True)
 p_H = (n_Am[0] / 2 - i_Am) / sl_Am
 d_p_H = p_H * sqrt((d_n_Am[0]**2 + 4 * d_i_Am**2) / (n_Am[0] - 2 * i_Am)**2 + (d_sl_Am / sl_Am)**2)
